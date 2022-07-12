2d11038b035f49044527dfd4759443871690a6f1
SHYAM JOSHI <sjuicyygmail.com@SAMMYs-MacBook-Air.local>
2022-07-12 14:16:55 +0545

    added 4 views

--- product/views.py
+++ product/views.py
@@ -3,4 +3,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_page(*args, **kwargs):
-    return HttpResponse("<h1>this is homepage<h1>")
+    return HttpResponse("<h1>this is home page<h1>")
+
+def name_page(*args, **kwargs):
+    return HttpResponse("<h1>this is name page<h1>")
+    
+def contact_page(*args, **kwargs):
+    return HttpResponse("<h1>this is contact page<h1>")
+
+def report_page(*args, **kwargs):
+    return HttpResponse("<h1>this is report page<h1>")    
