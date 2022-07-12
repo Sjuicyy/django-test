ea257edc715888e7f9380150c9f2cc697b6c8266
SHYAM JOSHI <sjuicyygmail.com@SAMMYs-MacBook-Air.local>
2022-07-12 14:06:01 +0545

    homepage added

--- product/views.py
+++ product/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def home_page(*args, **kwargs):
+    return HttpResponse("<h1>this is homepage<h1>")
