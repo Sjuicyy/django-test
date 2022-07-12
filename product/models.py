fe93a053a5638a1f53587ec9cea2c59996ff399c
SHYAM JOSHI <sjuicyygmail.com@SAMMYs-MacBook-Air.local>
2022-07-12 11:15:05 +0545

    2 new object

--- product/models.py
+++ product/models.py
@@ -1,6 +1,9 @@
+from xml.etree.ElementTree import Comment
 from django.db import models
 
 # Create your models here.
 class product(models.Model):
     name = models.TextField()
     price =models.TextField()
+    summary=models.TextField()
+    Comment=models.TextField()
