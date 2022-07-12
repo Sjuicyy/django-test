fd6a9f874c9fdaba9296f478491b86bab8fda08c
SHYAM JOSHI <sjuicyygmail.com@SAMMYs-MacBook-Air.local>
2022-07-12 10:41:05 +0545

    django

--- project1/settings.py
+++ project1/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'project1.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db222.sqlite3',
+        'NAME': BASE_DIR / 'db2.sqlite3',
     }
 }
 
